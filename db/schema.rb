8423b5f153c5bada990e68b197d281a5677c0af7
Nicholas Utikal <nicholasutikal@gmail.com>
2019-01-21 18:09:59 +0100

    adds jobs and model with docker

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20190121170317) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "counters", force: :cascade do |t|
+    t.integer  "total"
+    t.integer  "attempt"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
